cd1867a8b077fd4511b293290e69fc9f9f9f8eee
paralexdisha7 <paraledisha20@gmail.com>
2024-02-21 01:10:08 +0530

    ready to deply

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -76,7 +76,7 @@ const signup = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: createdUser.id, email: createdUser.email },
-      "supersecret_dont_share",
+      process.env.JWT_KEY,
       { expiresIn: "1h" }
     );
   } catch (err) {
@@ -135,7 +135,7 @@ const login = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: existingUser.id, email: existingUser.email },
-      "supersecret_dont_share",
+      process.env.JWT_KEY,
       { expiresIn: "1h" }
     );
   } catch (err) {
