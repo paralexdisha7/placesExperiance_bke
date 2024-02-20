b1f94d5db6bc9ad9175de47670a5b61f321a320c
paralexdisha7 <paraledisha20@gmail.com>
2024-02-20 22:46:43 +0530

    add auth

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -147,6 +147,12 @@ const updatePlace = async (req, res, next) => {
     );
     return next(error);
   }
+  if(place.creator.toString() !== req.userData.userId){
+    const error = new HttpError(
+      'You are not allowed to edit this place',
+      401
+    )
+  }
 
   place.title = title;
   place.description = description;
@@ -181,6 +187,13 @@ const deletePlace = async (req, res, next) => {
   if (!place) {
     const error = new HttpError("Could not find place for this id.", 404);
     return next(error);
+  }
+
+  if(place.creator.id !== req.userData.userId){
+    const error = new HttpError(
+      'You are not allowed to delete this place',
+      401
+    )
   }
 
   const imagePath = place.image;
--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,27 +1,28 @@
-const { validationResult } = require('express-validator');
-
-const HttpError = require('../models/http-error');
-const User = require('../models/user');
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const HttpError = require("../models/http-error");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const getUsers = async (req, res, next) => {
   let users;
   try {
-    users = await User.find({}, '-password');
+    users = await User.find({}, "-password");
   } catch (err) {
     const error = new HttpError(
-      'Fetching users failed, please try again later.',
+      "Fetching users failed, please try again later.",
       500
     );
     return next(error);
   }
-  res.json({ users: users.map(user => user.toObject({ getters: true })) });
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(
-      new HttpError('Invalid inputs passed, please check your data.', 422)
+      new HttpError("Invalid inputs passed, please check your data.", 422)
     );
   }
   const { name, email, password } = req.body;
@@ -31,7 +32,7 @@ const signup = async (req, res, next) => {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
     const error = new HttpError(
-      'Signing up failed, please try again later.',
+      "Signing up failed, please try again later.",
       500
     );
     return next(error);
@@ -39,9 +40,17 @@ const signup = async (req, res, next) => {
 
   if (existingUser) {
     const error = new HttpError(
-      'User exists already, please login instead.',
+      "User exists already, please login instead.",
       422
     );
+    return next(error);
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError("could not create user. Try Again", 500);
     return next(error);
   }
 
@@ -49,21 +58,35 @@ const signup = async (req, res, next) => {
     name,
     email,
     image: req.file.path,
-    password,
-    places: []
+    password: hashedPassword,
+    places: [],
   });
 
   try {
     await createdUser.save();
   } catch (err) {
     const error = new HttpError(
-      'Signing up failed, please try again later.',
+      "Signing up failed, please try again later.",
       500
     );
     return next(error);
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      "supersecret_dont_share",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      "Signing up failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+  res.status(201).json({ user: createdUser.id, email: createdUser.email,token:token });
 };
 
 const login = async (req, res, next) => {
@@ -75,23 +98,58 @@ const login = async (req, res, next) => {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
     const error = new HttpError(
-      'Loggin in failed, please try again later.',
+      "Loggin in failed, please try again later.",
       500
     );
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
     const error = new HttpError(
-      'Invalid credentials, could not log you in.',
-      401
+      "Invalid credentials, could not log you in.",
+      403
     );
     return next(error);
   }
 
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      403
+    );
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      "supersecret_dont_share",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError(
+      "Logging in failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+ 
   res.json({
-    message: 'Logged in!',
-    user: existingUser.toObject({ getters: true })
+   userId:existingUser.id,
+   email:existingUser.email,
+   token:token
   });
 };
 
