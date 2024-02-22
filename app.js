8957bd0bf2f7ae34954177f2951e5f0b63354713
paralexdisha7 <paraledisha20@gmail.com>
2024-02-22 11:38:50 +0530

    production ready

--- app.js
+++ app.js
@@ -1,56 +1,61 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const fs = require('fs');
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes');
-const HttpError = require('./models/http-error');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+const path = require("path");
 const app = express();
+
+const dotenv = require('dotenv').config();
+
+const DATABASE= process.env.MONGODB_URI;
 
 app.use(bodyParser.json());
 
-app.use('/uploads/images',express.static(path.join('uploads','images')))
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
   next();
 });
 
-app.use('/api/places', placesRoutes);
-app.use('/api/users', usersRoutes);
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
 app.use((error, req, res, next) => {
   if (req.file) {
-    fs.unlink(req.file.path,()=>{
+    fs.unlink(req.file.path, () => {
       console.log(error);
-    })    
+    });
   }
   if (res.headerSent) {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || 'An unknown error occurred!' });
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
 mongoose
-  .connect(
-    `mongodb+srv://paraledisha20:Disha@cluster0.psgklz7.mongodb.net/mern?retryWrites=true&w=majority`,{ useNewUrlParser: true ,useUnifiedTopology: true}
-  )
+  .connect( DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(process.env.PORT || 5000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
