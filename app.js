4540a69a3b441adc00a0033ff22a595eeba64b33
paralexdisha7 <paraledisha20@gmail.com>
2024-02-22 09:30:06 +0530

    deploy changes

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.psgklz7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{ useNewUrlParser: true ,useUnifiedTopology: true}
+    `mongodb+srv://paraledisha20:Disha@cluster0.psgklz7.mongodb.net/mern?retryWrites=true&w=majority`,{ useNewUrlParser: true ,useUnifiedTopology: true}
   )
   .then(() => {
     app.listen(process.env.PORT || 5000);
